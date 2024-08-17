7bc656b35ab1182761b54fef85ccf9cad6c8bd39
kvfs <sepixcraft48@gmail.com>
2024-08-17 03:16:59 +0200

    First working version of Kernel Driver :yipee:

--- Driver/src/main.cpp
+++ Driver/src/main.cpp
@@ -1,16 +1,13 @@
 #include <ntifs.h>
 
-PCWSTR DriverName   = L"\\Driver\\DexterionDriver";
-PCWSTR SymbolicName = L"\\DosDevices\\DexterionDriver";
-
 extern "C" {
 	NTKERNELAPI NTSTATUS IoCreateDriver(PUNICODE_STRING DriverName,
-										PDRIVER_INITIALIZE InitializationFunction);
+		PDRIVER_INITIALIZE InitializationFunction);
 
 	NTKERNELAPI NTSTATUS MmCopyVirtualMemory(PEPROCESS SourceProcess, PVOID SourceAddress,
-											PEPROCESS TargetProcess, PVOID TargetAddress,
-											SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode,
-											PSIZE_T ReturnSize);
+		PEPROCESS TargetProcess, PVOID TargetAddress,
+		SIZE_T BufferSize, KPROCESSOR_MODE PreviousMode,
+		PSIZE_T ReturnSize);
 }
 
 void debug_print(PCSTR text) {
@@ -23,8 +20,8 @@ void debug_print(PCSTR text) {
 namespace Driver {
 	namespace Codes {
 		constexpr ULONG Attach = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x696, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
-		constexpr ULONG Read   = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
-		constexpr ULONG Write  = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG Read = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x697, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
+		constexpr ULONG Write = CTL_CODE(FILE_DEVICE_UNKNOWN, 0x698, METHOD_BUFFERED, FILE_SPECIAL_ACCESS);
 	}
 
 	struct Request {
@@ -98,11 +95,11 @@ namespace Driver {
 	}
 }
 
-NTSTATUS DriverEntry_(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
+NTSTATUS driver_main(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_path) {
 	UNREFERENCED_PARAMETER(registry_path);
 
 	UNICODE_STRING driver_name = {};
-	RtlInitUnicodeString(&driver_name, DriverName);
+	RtlInitUnicodeString(&driver_name, L"\\Device\\DexterionDriver");
 
 	PDEVICE_OBJECT device_object = nullptr;
 	NTSTATUS status = IoCreateDevice(driver_object, 0, &driver_name, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &device_object);
@@ -115,7 +112,7 @@ NTSTATUS DriverEntry_(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_pat
 	debug_print("[+] Driver device succesfully created.\n");
 
 	UNICODE_STRING symbolic_link = {};
-	RtlInitUnicodeString(&symbolic_link, SymbolicName);
+	RtlInitUnicodeString(&symbolic_link, L"\\DosDevices\\DexterionDriver");
 
 	status = IoCreateSymbolicLink(&symbolic_link, &driver_name);
 
@@ -128,8 +125,8 @@ NTSTATUS DriverEntry_(PDRIVER_OBJECT driver_object, PUNICODE_STRING registry_pat
 
 	SetFlag(device_object->Flags, DO_BUFFERED_IO);
 
-	driver_object->MajorFunction[IRP_MJ_CREATE        ] = Driver::Create       ;
-	driver_object->MajorFunction[IRP_MJ_CLOSE         ] = Driver::Close        ;
+	driver_object->MajorFunction[IRP_MJ_CREATE] = Driver::Create;
+	driver_object->MajorFunction[IRP_MJ_CLOSE] = Driver::Close;
 	driver_object->MajorFunction[IRP_MJ_DEVICE_CONTROL] = Driver::DeviceControl;
 
 	ClearFlag(device_object->Flags, DO_DEVICE_INITIALIZING);
@@ -143,7 +140,7 @@ NTSTATUS DriverEntry() {
 	debug_print("[+] Hello world!\n");
 
 	UNICODE_STRING driver_name = {};
-	RtlInitUnicodeString(&driver_name, DriverName);
+	RtlInitUnicodeString(&driver_name, L"\\Driver\\DexterionDriver");
 
-	return IoCreateDriver(&driver_name, &DriverEntry_);
+	return IoCreateDriver(&driver_name, &driver_main);
 }
